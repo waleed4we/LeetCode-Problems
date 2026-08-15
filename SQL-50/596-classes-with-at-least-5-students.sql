3f5b89e1cd2b9869c7fbf23c83a79cf3f3c654a3
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-15 10:00:00 +0500

    Update 596-classes-with-at-least-5-students.sql

--- SQL-50/596-classes-with-at-least-5-students.sql
+++ SQL-50/596-classes-with-at-least-5-students.sql
@@ -1,1 +1,18 @@
+-- LeetCode #596 , Problem: Classes With at Least 5 Students
+-- Difficulty: Easy
 
+-- Table:
+-- Courses: student, class
+
+-- Task:
+-- Find all classes that have at least five students.
+
+-- Approach:
+-- 1. Group the Courses table by class.
+-- 2. Count the number of students in each class.
+-- 3. Use HAVING to keep only classes with at least 5 students.
+
+SELECT class
+FROM Courses
+GROUP BY class
+HAVING COUNT(student) >= 5;
