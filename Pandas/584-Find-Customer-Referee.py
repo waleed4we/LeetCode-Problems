24b2da3962442c61d0d8c8d9481ded1a051635ff
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-18 09:34:42 +0500

    Update 584-Find-Customer-Referee.py

--- Pandas/584-Find-Customer-Referee.py
+++ Pandas/584-Find-Customer-Referee.py
@@ -1,1 +1,20 @@
+# LeetCode #584, Problem: Find Customer Referee
+# Difficulty: Easy
 
+# Task:
+# Find customers who were not referred by the customer with id 2
+# Also include customers who were not referred by anyone
+
+import pandas as pd
+
+def find_customer_referee(customer: pd.DataFrame) -> pd.DataFrame:
+
+    return customer[
+        (customer['referee_id'] != 2) | (customer['referee_id'].isna())
+    ][['name']]
+
+# Approach :
+# 1. Filter customers whose referee_id is not equal to 2
+# 2. Include customers whose referee_id is null
+# 3. Combine both conditions using the OR operator
+# 4. Return only the name column
