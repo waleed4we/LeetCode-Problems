f62bb6409fa65af16e269a9c7d9e5ea193f7a9f5
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-18 22:01:16 +0500

    Update 1978-Employees-Whose-Manager-Left-The-Company.sql

--- SQL-50/1978-Employees-Whose-Manager-Left-The-Company.sql
+++ SQL-50/1978-Employees-Whose-Manager-Left-The-Company.sql
@@ -1,1 +1,19 @@
+-- LeetCode #1978, Problem: Employees Whose Manager Left the Company
+-- Difficulty: Easy
 
+-- Task:
+-- Find employees whose salary is less than 30000
+-- and whose manager has left the company
+
+select employee_id
+from employees
+where salary < 30000
+    and manager_id not in (
+      select employee_id from employees
+  )
+order by employee_id;
+
+-- Approach :
+-- 1. Filter employees whose salary is less than 30000
+-- 2. Check if their manager_id does not exist in the Employees table
+-- 3. Order the result by employee_id
