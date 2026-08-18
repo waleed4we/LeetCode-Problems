2dd0d27ff1b28d0e502a9cc262b579e0c8c28120
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-18 21:53:10 +0500

    Update 1075-Project-Employees-I.sql

--- SQL-50/1075-Project-Employees-I.sql
+++ SQL-50/1075-Project-Employees-I.sql
@@ -1,22 +1,21 @@
-# LeetCode #1075, Problem: Project Employees I
-# Difficulty: Easy
+-- LeetCode #1075 , Problem: Project Employees I
+-- Difficulty: Easy
 
-# Table:
-# Project: project_id, employee_id
-# Employee: employee_id, name, experience_years
+-- Table:
+-- Project: project_id, employee_id
+-- Employee: employee_id, name, experience_years
 
-# Task:
-# Find the average experience years of employees for each project
-# Round the average to 2 decimal places
+-- Task:
+-- Find the average experience years of employees for each project & Round the average to 2 decimal places
 
-select project.project_id ,
+select project.project_id,
        round(avg(employee.experience_years), 2) as average_years
 from project join employee
     on project.employee_id = employee.employee_id
 group by project.project_id;
 
-# Approach :
-# 1. Join Project and Employee using employee_id
-# 2. Group the employees by project_id
-# 3. Use avg() to calculate the average experience years for each project
-# 4. Use round() to round the average to 2 decimal places
+-- Approach:
+-- 1. Join Project and Employee using employee_id
+-- 2. Group the employees by project_id
+-- 3. Calculate the average experience years for each project
+-- 4. Round the average to 2 decimal places
