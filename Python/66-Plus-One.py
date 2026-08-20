90d41b9ee276e67300addcc461825c7fc4849ef2
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-20 22:09:36 +0500

    Update 66-Plus-One.py

--- Python/66-Plus-One.py
+++ Python/66-Plus-One.py
@@ -1,1 +1,18 @@
+class Solution(object):
+    def plusOne(self, digits):
+        my_str = ""
 
+        for i in digits:
+            my_str += str(i)
+
+        print(my_str)
+        my_int = int(my_str)
+        incremented_int = my_int + 1
+        incremented_str = str(incremented_int)
+
+        new_list = []
+        for i in incremented_str:
+            new_int_con = int(i)
+            new_list.append(new_int_con)
+
+        return new_list
