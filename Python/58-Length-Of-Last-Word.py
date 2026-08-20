9233c9db520aaab49331c55aa0c222db1aa465ee
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-20 22:02:38 +0500

    Update 58-Length-Of-Last-Word.py

--- Python/58-Length-Of-Last-Word.py
+++ Python/58-Length-Of-Last-Word.py
@@ -1,1 +1,19 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        my_s = s.strip()
+        count_spaces = 0
+        characters_count = 0
 
+        for i in range(len(my_s)):
+            characters_count += 1
+            if my_s[i] == " ":
+                count_spaces += 1
+
+        my_str = ""
+
+        for i in range(len(my_s) - 1, -1, -1):
+            if my_s[i] == " ":
+                break
+            my_str += my_s[i]
+
+        return len(my_str)
