34a304edc6281f027d0a26d36966b0790546cf69
Malik Waleed Hussian <waleed4we@gmail.com>
2026-08-20 21:39:30 +0500

    Refactor SQL query for clarity and efficiency

--- SQL-50/585-Investments-In-2016.sql
+++ SQL-50/585-Investments-In-2016.sql
@@ -12,14 +12,12 @@
 select sum(tiv_2016) as tiv_2016
 from insurance
 where tiv_2015 in (
-    select tiv_2015
-    from insurance
+    select tiv_2015 from insurance
     group by tiv_2015
     having count(*) > 1
 )
 and (lat, lon) in (
-    select lat, lon
-    from insurance
+    select lat, lon from insurance
     group by lat, lon
     having count(*) = 1
 );
